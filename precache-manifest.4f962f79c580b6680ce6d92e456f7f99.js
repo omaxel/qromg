73f5aab3ea4eab5b5d0006d8dbb114a787a7e685
Omar Muscatello <7016897+OmarMuscatello@users.noreply.github.com>
2020-02-03 23:03:55 +0100

    Updates

---
+++ precache-manifest.4f962f79c580b6680ce6d92e456f7f99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4530dbd9faec8e81eec470533f7749",
+    "url": "/qromg/9a3633351a1a96cf06a9.worker.js"
+  },
+  {
+    "revision": "01d6505bcda0fb5ed16fe1115bb93d0e",
+    "url": "/qromg/index.html"
+  },
+  {
+    "revision": "bb542376252ce6cc490a",
+    "url": "/qromg/static/css/main.67a38333.chunk.css"
+  },
+  {
+    "revision": "51370f4bb2872b31726d",
+    "url": "/qromg/static/js/2.d4a8e847.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/qromg/static/js/2.d4a8e847.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb542376252ce6cc490a",
+    "url": "/qromg/static/js/main.6dabba5c.chunk.js"
+  },
+  {
+    "revision": "f2faa9a62bde94ee1b47",
+    "url": "/qromg/static/js/runtime-main.f8efde7c.js"
+  }
+]);
--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { classNames } from '../../utils';
-import classes from './Button.module.scss';
-
-export default ({ className, children, ...rest }) => (
-    <button className={classNames(classes.root, className)} {...rest}>
-        {children}
-    </button>
-);
